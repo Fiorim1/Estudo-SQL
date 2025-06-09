b2872c81c0ea7705813c2487b9be7ce3a6a4200b
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-06-09 09:24:06 -0300

    Update Joins-UniaoEntreTabelas.sql

--- banco de dados (estudo)/Joins-UniaoEntreTabelas.sql
+++ banco de dados (estudo)/Joins-UniaoEntreTabelas.sql
@@ -5,3 +5,13 @@ SELECT * FROM pedidos
 INNER JOIN clientes ON pedidos.cliente_id = clientes.id;
 
 /* LEFT JOIN: tudo da esquerda + correspondências da direita */
+SELECT * FROM pedidos 
+LEFT JOIN clientes ON pedidos.cliente_id = cliente.id;
+
+/* RIGHT JOIN: tudo da direita + correspondências da esquerda */
+SELECT * FROM pedidos 
+RIGHT JOIN clientes ON pedidos.cliente_id = clientes.id;
+
+/* FULL JOIN: tudo das duas tabelas */
+SELECT * FROM pedidos 
+FULL JOIN clientes ON pedidos.cliente-id = clientes.id;
